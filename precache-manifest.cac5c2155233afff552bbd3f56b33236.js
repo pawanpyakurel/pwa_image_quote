755049eadeeb58c9af0ee187cb2996742e649286
pawanpyakurel30 <pawan.pyakurel@satyatech.xyz>
2020-06-27 22:24:19 +0545

    Updates

---
+++ precache-manifest.cac5c2155233afff552bbd3f56b33236.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8a6013e247cc467fe0255e1b9e11da",
+    "url": "/pwa_image_quote/index.html"
+  },
+  {
+    "revision": "f627387dc8e150b637dc",
+    "url": "/pwa_image_quote/static/js/2.8a306882.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pwa_image_quote/static/js/2.8a306882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a2e32dbbaa8cc150104",
+    "url": "/pwa_image_quote/static/js/main.f10a01bc.chunk.js"
+  },
+  {
+    "revision": "7fae72eb3d82925afad1",
+    "url": "/pwa_image_quote/static/js/runtime-main.5322bd91.js"
+  }
+]);
